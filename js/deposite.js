5d1e8ca5f80bb390739fd3c85b7b59a9047ac422
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-02-12 16:23:53 +0600

    deposit done

---
+++ js/deposite.js
@@ -0,0 +1,15 @@
+document.getElementById("deposit-btn").addEventListener("click", function () {
+    const inputDepositFieldValue = parseFloat(document.getElementById("input-deposit-field").value);
+    const depositAmountDisplay = parseFloat(document.getElementById("deposit-amount").innerText);
+    const mainBalance = parseFloat(document.getElementById("main-balance").innerText);
+    document.getElementById("input-deposit-field").value = "";
+    if (isNaN(inputDepositFieldValue)) {
+        return alert("Plaese Provide Any Valid Amount")
+    }
+    const totalDeposit = inputDepositFieldValue + depositAmountDisplay;
+    const totalMainBalance = inputDepositFieldValue + mainBalance;
+    document.getElementById("deposit-amount").innerText = totalDeposit;
+    document.getElementById("main-balance").innerText = totalMainBalance;
+
+
+})
