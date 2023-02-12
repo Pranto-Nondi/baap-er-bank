da4cf1e7bf92934febca12e8fe3465727fd823a8
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-02-12 17:24:40 +0600

    done

--- js/login.js
+++ js/login.js
@@ -1,11 +1,15 @@
 document.getElementById("sumbit-btn").addEventListener("click", function () {
     const userEmailValue = document.getElementById("user-email").value;
     const userPasswordValue = document.getElementById("user-password").value;
+
     if (userEmailValue === "pranto.cse@gmail.com" && userPasswordValue === "pranto25") {
         window.location.href = "bank.html"
     }
     else {
-        alert("Please proved Correct user name or password")
+        document.getElementById("user-email").value = "";
+        document.getElementById("user-password").value = "";
+        alert("Please proved Correct user name or password");
+
     }
 
 
