68b00e621fb64956fe102b419b2d3ae73813b5a5
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-02-13 17:44:21 +0600

    done

--- js/functinalWay/deposit.js
+++ js/functinalWay/deposit.js
@@ -2,11 +2,15 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
     const getDepositInputValue = getInputValueById("input-deposit-field");
     const getDepositTextValue = getTextValueById("deposit-amount");
-    const setTotalDeposit = getDepositInputValue + getDepositTextValue;
-    console.log(setTotalDeposit)
-    setTextValue("deposit-amount", setTotalDeposit);
-    const getMainTextBalance = getTextValueById("main-balance");
-    const mainBalanceTotal = getMainTextBalance + getDepositInputValue;
-    setTextValue("main-balance", mainBalanceTotal);
-    console.log(mainBalanceTotal);
+    if (getDepositInputValue >= 0) {
+
+        const setTotalDeposit = getDepositInputValue + getDepositTextValue;
+        setTextValue("deposit-amount", setTotalDeposit);
+        const getMainTextBalance = getTextValueById("main-balance");
+        const mainBalanceTotal = getMainTextBalance + getDepositInputValue;
+        setTextValue("main-balance", mainBalanceTotal);
+    }
+    else {
+        alert("Please provide Correct Value")
+    }
 })
--- js/functinalWay/withdraw.js
+++ js/functinalWay/withdraw.js
@@ -2,14 +2,17 @@
 document.getElementById("withdraw-btn").addEventListener("click", function () {
     const getWithdrawInputValue = getInputValueById("input-withdraw-field");
     const getMainTextValue = getTextValueById("main-balance");
-    if (getWithdrawInputValue <= getMainTextValue) {
-
+    if (getWithdrawInputValue < 0) {
+        alert("Please Provide Correct Value")
+    }
+    else if (getWithdrawInputValue <= getMainTextValue) {
         const setTotalMainBalance = getMainTextValue - getWithdrawInputValue;
         setTextValue("main-balance", setTotalMainBalance);
         const getWithdrawTextValue = getTextValueById("withdraw-display");
         const setTotalWithdraw = getWithdrawInputValue + getWithdrawTextValue;
         setTextValue("withdraw-display", setTotalWithdraw);
     }
+
     else {
         alert("Sorry ! You Have Insufficient Balance");
     }
